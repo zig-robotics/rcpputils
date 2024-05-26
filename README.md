20dbfc3e114ca5f2eb33b9b7689908a15242fa61
zig-robotics <141579568+zig-robotics@users.noreply.github.com>
2024-05-26 12:11:55 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Zig package for rcpputils
+
+This provides a zig package for the rcpputils project.
+This currently targets zig 0.12 and ROS Iron.
+
+https://github.com/ros2/rcpputils/tree/iron
---
+++ build.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+
+    const optimize = b.standardOptimizeOption(.{});
+
+    const linkage = b.option(std.builtin.LinkMode, "linkage", "Specify static or dynamic linkage") orelse .dynamic;
+    const upstream = b.dependency("rcpputils", .{});
+    var lib = std.Build.Step.Compile.create(b, .{
+        .root_module = .{
+            .target = target,
+            .optimize = optimize,
+        },
+        .name = "rcpputils",
+        .kind = .lib,
+        .linkage = linkage,
+    });
+
+    lib.linkLibCpp();
+    lib.addIncludePath(upstream.path("include"));
+
+    const rcutils_dep = b.dependency("rcutils", .{
+        .target = target,
+        .optimize = optimize,
+        .linkage = linkage,
+    });
+
+    lib.linkLibrary(rcutils_dep.artifact("rcutils"));
+
+    lib.addCSourceFiles(.{
+        .root = upstream.path(""),
+        .files = &.{
+            "src/asserts.cpp",
+            "src/filesystem_helper.cpp",
+            "src/find_library.cpp",
+            "src/env.cpp",
+            "src/shared_library.cpp",
+        },
+        .flags = &[_][]const u8{
+            "--std=c++17",
+        },
+    });
+
+    lib.installHeadersDirectory(
+        upstream.path("include"),
+        "",
+        .{ .include_extensions = &.{".hpp"} },
+    );
+    b.installArtifact(lib);
+}
